4924bd9d3f694f3dadc8108acee8a5c83a709813
AsuraHu <1030320304@qq.com>
2019-11-25 11:19:18 +0800

    filish header

---
+++ src/main.js
@@ -0,0 +1,25 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import fastClick from 'fastclick'
+// import Element from 'element-ui'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import 'styles/reset.css'
+import 'styles/border.css'
+
+Vue.use(ElementUI)
+Vue.config.productionTip = false
+// Vue.use(Element, { size: 'small', zIndex: 3000 })
+fastClick.attach(document.body)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  // render: h => h(App),
+  router,
+  components: { App },
+  template: '<App/>'
+})
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <i class="el-icon-arrow-left" style="font-size: 24px;"></i>
+    </div>
+    <div class="header-input">
+      <i class="el-icon-search" style="font-size: 18px;"></i>
+      <span>输入城市游玩主题</span>
+    </div>
+    <div class="header-right">
+      <span class="right-text">城市</span>
+      <i class="el-icon-caret-bottom" style="font-size: 20px;"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+  @import '~styles/varibles.styl'
+  .header
+    height: .86rem
+    line-height: .86rem
+    display: flex
+    background-color: $bgcolor
+    color: #fff
+    .header-left
+      width: .64rem
+      float: left
+      .el-icon-arrow-left
+        display: block
+        height: .86rem
+        line-height: .86rem
+        text-align: center
+    .header-right
+      width: 1.24rem
+      float: right
+      text-align: center
+      .right-text
+        display: inline-block
+      .el-icon-caret-bottom
+        display: inline-block
+        margin-left: -.08rem
+    .header-input
+      flex: 1
+      height: .64rem
+      line-height: .64rem
+      margin-top: .11rem
+      margin-left: .2rem
+      padding-left: .2rem
+      color: #ccc
+      background-color: #fff
+      border-radius: .1rem
+</style>
