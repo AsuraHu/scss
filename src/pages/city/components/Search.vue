e47d3e1adc4d6b406988075371315382fdecd678
AsuraHu <1030320304@qq.com>
2019-11-26 15:09:59 +0800

    城市也面的搜索框

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="search">
+    <input class="search-input" type="text" placeholder="输入城市名或者拼音">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style scoped lang="stylus">
+  @import '~styles/varibles.styl'
+  .search
+    height: .72rem
+    padding: 0 .1rem
+    background-color: $bgcolor
+    .search-input
+      width: 100%
+      height: .62rem
+      padding: 0 .2rem
+      box-sizing: border-box
+      line-height: .62rem
+      text-align: center
+      border-radius: .06rem
+</style>
