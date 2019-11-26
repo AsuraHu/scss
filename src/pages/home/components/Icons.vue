9f00c9ee438b38fe018e2589c29b1fbe54055874
AsuraHu <1030320304@qq.com>
2019-11-26 09:28:06 +0800

    finished icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="icons">
+    <swiper>
+      <swiper-slide v-for="(page, index) of pages" :key="index">
+        <div class="icon"
+          v-for="item of page"
+          :key="item.id"
+        >
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl">
+          </div>
+          <p class="icon-title">{{item.text}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [{
+        id: '0001',
+        text: '景点门票',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png'
+      }, {
+        id: '0002',
+        text: '成都海昌',
+        imgUrl: 'https://img1.qunarzz.com/piao/fusion/1810/50/26ffa31b56646402.png'
+      }, {
+        id: '0003',
+        text: '情迷九寨',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/97/02f5043b51b2102.png'
+      }, {
+        id: '0004',
+        text: '川剧变脸',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/e3/67df61427c8e1302.png'
+      }, {
+        id: '0005',
+        text: '一日游',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png'
+      }, {
+        id: '0006',
+        text: '国色天香',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/8246f27355943202.png'
+      }, {
+        id: '0007',
+        text: '成都欢乐谷',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/338c5b924c5809e8c7b14f60a953c3e2.png'
+      }, {
+        id: '0008',
+        text: '熊猫基地',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/54/35899492b1302802.png'
+      }, {
+        id: '0009',
+        text: '青山城',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20197/02bbb71bd1f629c2348d0c97ef82c129.png'
+      }, {
+        id: '00010',
+        text: '都江堰',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/f04285731d7121da1b9028e2bf431695.png'
+      }]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+  @import '~styles/varibles.styl'
+  @import '~styles/mixins.styl'
+  .icons >>> .swiper-container
+    height: 0rem
+    padding-bottom: 50%
+  .icon
+    position: relative
+    overflow: hidden
+    float: left
+    width: 25%
+    height: 0
+    padding-bottom: 25%
+    margin-bottom: .05rem
+    .icon-img
+      position: absolute
+      top: 0
+      left: 0
+      right: 0
+      bottom: .44rem
+      box-sizing: border-box
+      .icon-img-content
+        display: block
+        margin: 0 auto
+        height: 100%
+    .icon-title
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      height: .44rem
+      line-height: .44rem
+      text-align: center
+      color: $darkTextColor
+      ellipsis()
+</style>
