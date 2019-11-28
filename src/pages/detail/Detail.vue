2220a9f2c16d243aa5beea87bb1a9bcd94e51320
AsuraHu <1030320304@qq.com>
2019-11-28 14:53:27 +0800

    多级列表

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,18 +2,43 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
-    <div class="content"></div>
+    <div class="content">
+      <detail-list :list="list"></detail-list>
+    </div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      list: [{
+        title: '成人票',
+        children: [{
+          title: '成人2-1'
+        }, {
+          title: '成人2-2',
+          children: [{
+            title: '成人2-2-1'
+          }]
+        }]
+      }, {
+        title: '儿童票票'
+      }, {
+        title: '优惠票'
+      }, {
+        title: '半价票'
+      }]
+    }
   }
 }
 </script>
--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -61,6 +61,7 @@ export default {
     background-color: rgba(0, 0, 0, .8)
     color: #fff
   .header-fixed
+    z-index: 9
     position: fixed
     top: 0
     left: 0
